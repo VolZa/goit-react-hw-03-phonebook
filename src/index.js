99879a9f18ce8f699d0c18e1c405a43ec5c2bc0a
VolZa <vvolza@gmail.com>
2023-05-05 19:34:42 +0300

    додав BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/app/App';
 // import './index.css';rt
 import { ThemeProvider } from 'styled-components';
@@ -15,7 +17,9 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter basename="/goit-react-hw-03-phonebook">
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
